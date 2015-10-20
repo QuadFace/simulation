4d452d71d70d9b80ad64d7472e1238a4ade684ab
Pranay Prakash <pranay.gp@gmail.com>
2015-10-20 03:09:10 -0500

    Getting live data from arduino and logging it on browser console

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var SerialPort = require("serialport").SerialPort;
+var serialPort = new SerialPort("/dev/tty.usbmodem1411", {
+  baudrate: 9600,
+  stopbits: 1,
+  databits: 7
+});
+
+var buffer = "";
+
 app.get('/', function(req, res){
 	res.sendfile(__dirname + "/quadface.html");
 });
@@ -13,9 +22,17 @@ io.on('connection',function(socket){
 	console.log('Someone connected');
 });
 
-setInterval(function(){
-	io.emit("data", 10);
-}, 1000);
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    buffer += data;
+    if (data.toString().indexOf("\n") > -1) {
+    	io.emit("data", buffer);
+    	buffer = "";
+    };
+
+  });
+});
 
 http.listen(3000, function(){
 	console.log("Listening on port 3000");
