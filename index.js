6f07d8ec6849e61c7be0dbda0ecf1155ffab1b73
Pranay Prakash <pranay.gp@gmail.com>
2015-10-20 02:34:48 -0500

    Included socket.io and made a onConnection event handler on server side

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 	res.sendfile(__dirname + "/quadface.html");
 });
 
 app.use("/", express.static(__dirname + '/'));
+
+io.on('connection',function(socket){
+	console.log('Someone connected');
+});
 
 http.listen(3000, function(){
 	console.log("Listening on port 3000");
