97f7f6cec21426a13ae12af308a3c8f040109913
Pranay Prakash <pranay.gp@gmail.com>
2015-10-20 02:26:45 -0500

    Serving up quadface.html instead of Hello World text

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-	res.send("<h1>Hello World</h1>");
+	res.sendfile(__dirname + "/quadface.html");
 });
+
+app.use("/css", express.static(__dirname + '/css'));
+
+app.use("/assets", express.static(__dirname + '/assets'));
 
 http.listen(3000, function(){
 	console.log("Listening on port 3000");
