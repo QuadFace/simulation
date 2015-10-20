1fca14d35dbd4302232159e4a225a0d18d4c166f
Pranay Prakash <pranay.gp@gmail.com>
2015-10-20 02:31:08 -0500

    Refactored code to serve any static file in directory

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ app.get('/', function(req, res){
 	res.sendfile(__dirname + "/quadface.html");
 });
 
-app.use("/css", express.static(__dirname + '/css'));
-
-app.use("/assets", express.static(__dirname + '/assets'));
+app.use("/", express.static(__dirname + '/'));
 
 http.listen(3000, function(){
 	console.log("Listening on port 3000");
