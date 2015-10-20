726e7cef1026ac0729954f29384d8f3edc990ef4
Pranay Prakash <pranay.gp@gmail.com>
2015-10-20 02:07:53 -0500

    Added socket.io include on vlient end and created package.json

---
+++ test.js
@@ -0,0 +1,24 @@
+var SerialPort = require("serialport").SerialPort;
+var serialPort = new SerialPort("/dev/tty.usbmodem1411", {
+  baudrate: 9600,
+  stopbits: 1,
+  databits: 7
+});
+
+var buffer = "";
+
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    buffer += data;
+    if (data.toString().indexOf("\n") > -1) {
+    	console.log("[" + buffer + "]");
+    	buffer = "";
+    };
+
+  });
+  serialPort.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+});
